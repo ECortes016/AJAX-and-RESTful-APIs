c775bd5b2aaa345897498dc966b36444ffd6975c
Emmanuel Cortes <ecortes016@west-mec.org>
2019-09-12 18:26:52 -0700

    Updated script
    Added some script

--- 02-AJAX/Exercise-LAMP/www/solar.php
+++ 02-AJAX/Exercise-LAMP/www/solar.php
@@ -1,4 +1,5 @@
 <?php
+// Declare a variable with a $ sign
 $WeatherSource = "https://api.forecast.io/forecast/a465f4ec052a8289160841831b12d044/" . $_GET["lat"] . "," . $_GET["lng"];
 header("Content-Type: application/json");
 header("Cache-Control: no-cache");
